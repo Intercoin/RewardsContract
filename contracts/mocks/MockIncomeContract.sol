cea0408a87bf18a2dddd69a1097a84bed397c6c2
artman325 <subbotin.artem@gmail.com>
2022-04-26 18:02:48 +0300

    submodule IncomeContract added (#16)

---
+++ contracts/mocks/MockIncomeContract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.11;
+
+import "../../submodules/IncomeContract/contracts/IncomeContractUBILinear.sol";
+
+contract MockIncomeContract is IncomeContractUBILinear {
+    
+}
